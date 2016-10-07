6e3c47e8116cedc3a897920b1b4b813e2d087275
Danny Hermes <daniel.j.hermes@gmail.com>
2016-10-06 21:26:37 -0700

    Adding basic functionality detecting Travis environment.

--- ci_diff_helper/__init__.py
+++ ci_diff_helper/__init__.py
@@ -9,3 +9,85 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""Diff Helper for Continuous Integration (CI) Services.
+
+Provides a set of utilities for dealing with Travis CI.
+"""
+
+
+import os
+
+
+_IN_TRAVIS_ENV = 'TRAVIS'
+_TRAVIS_PR_ENV = 'TRAVIS_PULL_REQUEST'
+_TRAVIS_BRANCH_ENV = 'TRAVIS_BRANCH'
+
+
+def in_travis():
+    """Detect if we are running in Travis.
+
+    .. _Travis env docs: https://docs.travis-ci.com/user/\
+                         environment-variables\
+                         #Default-Environment-Variables
+
+    See `Travis env docs`_.
+
+    :rtype: bool
+    :returns: Flag indicating if we are running on Travis.
+    """
+    return os.getenv(_IN_TRAVIS_ENV) == 'true'
+
+
+def in_travis_pr():
+    """Detect if we are running in a pull request on Travis.
+
+    .. _Travis env docs: https://docs.travis-ci.com/user/\
+                         environment-variables\
+                         #Default-Environment-Variables
+
+    See `Travis env docs`_.
+
+    .. note::
+
+        This assumes we already know we are running in Travis.
+
+    :rtype: bool
+    :returns: Flag indicating if we are in a pull request on Travis.
+    """
+    # NOTE: We're a little extra cautious and make sure that the
+    #       PR environment variable is an integer.
+    try:
+        int(os.getenv(_TRAVIS_PR_ENV, ''))
+        return True
+    except ValueError:
+        return False
+
+
+def travis_branch():
+    """Get the current branch of the PR.
+
+    .. _Travis env docs: https://docs.travis-ci.com/user/\
+                         environment-variables\
+                         #Default-Environment-Variables
+
+    See `Travis env docs`_.
+
+    .. note::
+
+        This assumes we already know we are running in Travis
+        during a PR.
+
+    :rtype: str
+    :returns: The name of the branch the current pull request is
+              changed against.
+    :raises: :class:`~exceptions.OSError` if the ``_TRAVIS_BRANCH_ENV``
+             environment variable isn't set during a pull request
+             build.
+    """
+    try:
+        return os.environ[_TRAVIS_BRANCH_ENV]
+    except KeyError:
+        msg = ('Pull request build does not have an '
+               'associated branch set (via %s)') % (_TRAVIS_BRANCH_ENV,)
+        raise OSError(msg)
--- tests/test_it.py
+++ tests/test_it.py
@@ -13,10 +13,82 @@
 import unittest
 
 
-class TestLibrary(unittest.TestCase):
+class Test_in_travis(unittest.TestCase):
 
-    def test_import_works(self):
-        import types
+    @staticmethod
+    def _call_function_under_test():
+        from ci_diff_helper import in_travis
+        return in_travis()
+
+    def test_success(self):
+        import mock
         import ci_diff_helper
 
-        self.assertIsInstance(ci_diff_helper, types.ModuleType)
+        mock_env = {ci_diff_helper._IN_TRAVIS_ENV: 'true'}
+        with mock.patch('os.environ', new=mock_env):
+            self.assertTrue(self._call_function_under_test())
+
+    def test_failure(self):
+        import mock
+
+        with mock.patch('os.environ', new={}):
+            self.assertFalse(self._call_function_under_test())
+
+
+class Test_in_travis_pr(unittest.TestCase):
+
+    @staticmethod
+    def _call_function_under_test():
+        from ci_diff_helper import in_travis_pr
+        return in_travis_pr()
+
+    def test_success(self):
+        import mock
+        import ci_diff_helper
+
+        valid_int = '1234'
+        self.assertEqual(int(valid_int), 1234)
+        mock_env = {ci_diff_helper._TRAVIS_PR_ENV: valid_int}
+        with mock.patch('os.environ', new=mock_env):
+            self.assertTrue(self._call_function_under_test())
+
+    def test_failure_unset(self):
+        import mock
+
+        with mock.patch('os.environ', new={}):
+            self.assertFalse(self._call_function_under_test())
+
+    def test_failure_bad_value(self):
+        import mock
+        import ci_diff_helper
+
+        not_int = 'not-int'
+        self.assertRaises(ValueError, int, not_int)
+        mock_env = {ci_diff_helper._TRAVIS_PR_ENV: not_int}
+        with mock.patch('os.environ', new=mock_env):
+            self.assertFalse(self._call_function_under_test())
+
+
+class Test_travis_branch(unittest.TestCase):
+
+    @staticmethod
+    def _call_function_under_test():
+        from ci_diff_helper import travis_branch
+        return travis_branch()
+
+    def test_success(self):
+        import mock
+        import ci_diff_helper
+
+        branch = 'this-very-branch'
+        mock_env = {ci_diff_helper._TRAVIS_BRANCH_ENV: branch}
+        with mock.patch('os.environ', new=mock_env):
+            result = self._call_function_under_test()
+            self.assertEqual(result, branch)
+
+    def test_failure(self):
+        import mock
+
+        with mock.patch('os.environ', new={}):
+            with self.assertRaises(OSError):
+                self._call_function_under_test()
