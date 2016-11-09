fe5abc2e07c55abfd7c1bf497376c70284568010
Danny Hermes <daniel.j.hermes@gmail.com>
2016-11-09 10:11:37 -0800

    Using a README template to make PyPI docs simpler to change.

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@
 
 """Setup file for ci-diff-helper."""
 
-import collections
 import os
 
 from setuptools import find_packages
@@ -22,38 +21,20 @@ from setuptools import setup
 VERSION = '0.1.0'
 PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(PACKAGE_ROOT, 'README.rst')) as file_obj:
-    README = file_obj.read()
+with open(os.path.join(PACKAGE_ROOT, 'README.rst.template')) as file_obj:
+    TEMPLATE = file_obj.read()
 
-# Remove PyPI and Python versions badges from README since this
-# will show up on PyPI.
-README = README.replace(
-    '|pypi| |build| |appveyor| |coverage| |versions| |docs|',
-    '|build| |appveyor| |coverage| |docs|')
-BADGES_START = README.index('.. |build| image')
-BADGES_TEXT = '\n' + README[BADGES_START:].strip()
-# We know ``BADGES_TEXT`` starts with '\n.. |build' hence when we
-# split on '\n .. |' to find each badge's info.
-BADGES = collections.OrderedDict()
-for line in BADGES_TEXT.split('\n.. |')[1:]:
-    BADGE_NAME = line[:line.index('|')]
-    BADGES[BADGE_NAME] = '.. |' + line
-# Continue removing PyPI and Python versions badges.
-BADGES.pop('pypi')
-BADGES.pop('versions')
-# Update the Travis CI badge.
-BADGES['build'] = BADGES['build'].replace(
-    'branch=master', 'tag=' + VERSION)
-# Update the coveralls.io badge.
-BADGES['coverage'] = BADGES['coverage'].replace(
-    'branch=master', 'branch=' + VERSION)
-# Update the docs badge.
-BADGES['docs'] = BADGES['docs'].replace('latest', VERSION)
-# Update the AppVeyor badge.
-BADGES['appveyor'] = BADGES['appveyor'].replace(
-    'branch=master', 'tag=' + VERSION)
-# Update the README with the new tag content.
-README = README[:BADGES_START] + '\n'.join(BADGES.values()) + '\n'
+TAG_QUERY = 'tag={}'.format(VERSION)
+README = TEMPLATE.format(
+    pypi='',
+    pypi_img='',
+    versions='',
+    versions_img='',
+    travis_info=TAG_QUERY,
+    appveyor_info=TAG_QUERY,
+    coveralls_branch=VERSION,
+    rtd_version=VERSION,
+)
 
 REQUIREMENTS = (
     'enum34',
